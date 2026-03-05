06739159b36bfd0dd8a3f87e517cea7d93deddc5
Lukas <LukasHoltbrink@web.de>
2026-03-05 12:12:01 +0100

    fix price type

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,7 +3,7 @@ import AddToCartButton from "./add-to-cart-button";
 type ProductCardProps = {
   product: {
     title: string;
-    price: string;
+    price: number;
     imageUrl?: string;
   }
 }
@@ -17,7 +17,7 @@ export default function ProductCard({ product } : ProductCardProps) {
             className="w-full h-95 object-cover mb-2"
         />
         <h2 className="my-2">{product.title}</h2>
-        <p className="font-bold mb-3">{product.price} €</p>
+        <p className="font-bold mb-3">{product.price.toFixed(2)} €</p>
         <AddToCartButton product={product} />
     </div>
   )
